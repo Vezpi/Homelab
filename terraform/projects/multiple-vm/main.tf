285917f1045f3c0ce7afa296f93c49e5daca6141
Vezpi <etienne.girault@gmail.com>
2025-03-24 10:31:38 +0000

    replace static set with data source for node names

--- terraform/projects/multiple-vm/main.tf
+++ terraform/projects/multiple-vm/main.tf
@@ -1,12 +1,14 @@
 module "pve_vm" {
   source    = "../../modules/pve_vm"
-  for_each  = var.node_list
+  for_each = toset(data.proxmox_virtual_environment_nodes.pve_nodes.names)
   node_name = each.value
   vm_name   = "${each.value}-vm"
   vm_cpu    = 2
   vm_ram    = 2048
   vm_vlan   = 66
 }
+
+data "proxmox_virtual_environment_nodes"  "pve_nodes" {} 
 
 output "vm_ip" {
   value = { for k, v in module.pve_vm : "${k}-vm" => v.vm_ip }
