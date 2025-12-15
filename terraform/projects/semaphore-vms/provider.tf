1bffdef5fee94ca732625ce470a3b53158e1b221
Vezpi <etienne.girault@gmail.com>
2025-12-15 19:38:03 +0000

    revert: insecure to false

--- terraform/projects/semaphore-vms/provider.tf
+++ terraform/projects/semaphore-vms/provider.tf
@@ -9,7 +9,7 @@ terraform {
 provider "proxmox" {
   endpoint  = var.proxmox_endpoint
   api_token = var.proxmox_api_token
-  insecure  = true
+  insecure  = false
   ssh {
     agent       = false
     # private_key = file("~/.ssh/id_ed25519")
